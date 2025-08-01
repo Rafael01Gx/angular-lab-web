9e304c243f418214c7212a3931f193386bb2a4f9
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-08-01 20:12:14 -0300

    ref: Refatora services e adiciona interceptor para tratamento de erros. Inclui Toasts em componentes.

---
+++ src/app/core/interceptors/errors.interceptor.ts
@@ -0,0 +1,21 @@
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
+import {catchError, throwError} from 'rxjs';
+import {httpErrorMessages} from '../../shared/constants/http-error-messages';
+import {inject} from '@angular/core';
+import {ToastrService} from '../../components/layout/toastr/toastr.service';
+
+export const errorsInterceptor: HttpInterceptorFn = (req, next) => {
+  const toast = inject(ToastrService);
+  return next(req).pipe(
+    catchError((err:HttpErrorResponse)=>{
+      if(err.status < 408){
+        return throwError(()=>err);
+      }
+      const errorMessage = httpErrorMessages[err.status.toString()] || httpErrorMessages['default']
+      if(err.status >= 408){
+        toast.error(errorMessage);
+      }
+      return throwError(() => new Error(errorMessage))
+    })
+  );
+};
