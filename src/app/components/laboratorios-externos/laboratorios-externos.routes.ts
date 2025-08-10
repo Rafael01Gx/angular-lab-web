27593eeb81eb804830cb5a47bbf6f2ecf720bbf9
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-08-10 15:36:56 -0300

    feat: Adiciona rotas para Laboratórios Externos.

---
+++ src/app/components/laboratorios-externos/laboratorios-externos.routes.ts
@@ -0,0 +1,26 @@
+
+import { Routes } from '@angular/router';
+
+export const LABORATORIOS_EXTERNOS_ROUTES: Routes = [
+  {
+    path: 'remessa',
+    loadComponent: () =>
+      import(
+        './remessa/remessa.component'
+        ).then((m) => m.RemessaComponent),
+  },  {
+    path: 'amostras',
+    loadComponent: () =>
+      import(
+        './amostras/amostras.component'
+        ).then((m) => m.AmostrasComponent),
+  },
+  {
+    path: 'elementos-quimicos',
+    loadComponent: () =>
+      import(
+        './elementos-quimicos/elementos-quimicos.component'
+        ).then((m) => m.ElementosQuimicosComponent),
+  },
+
+]
